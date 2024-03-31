1f95b3e0fc7a375bdcf3983524163c814c05175e
pritam guha <pritam@tenxerlabs.com>
2024-03-31 18:59:47 +0530

    array method completed

--- Array-Methods/Array.js
+++ Array-Methods/Array.js
@@ -1,112 +1,112 @@
-// /*
-// Find Methods---------------------
-// -- Array Method is a kind of loop. Without array method we can use a for loop. It will start looping through every array element.
+/*
+Find Methods---------------------
+-- Array Method is a kind of loop. Without array method we can use a for loop. It will start looping through every array element.
 
-// -- Here, it will return only the first found element. It will not return multiple elements. 
-//    EX:-- ((i) => i === 'Mango' || i === 'Orange)  It will never Return 2nd element.
+-- Here, it will return only the first found element. It will not return multiple elements. 
+   EX:-- ((i) => i === 'Mango' || i === 'Orange)  It will never Return 2nd element.
 
-// -- ((i) => i === 'Mango') means: In here, I am declaring a function as a parameter. This is an arrow function, and it will 
-//    start looping through each element in the fruit array.
+-- ((i) => i === 'Mango') means: In here, I am declaring a function as a parameter. This is an arrow function, and it will 
+   start looping through each element in the fruit array.
 
-// -- findIndex -- It will give only the index of the elements.
-// */
-// const FindFruits = ['Banana', 'Orange', 'Mango', 'Apple']
-// const FindResult = FindFruits.find((i) => i === 'Mango');
-// const FindResultIndex = FindFruits.findIndex((i) => i === 'Mango');
+-- findIndex -- It will give only the index of the elements.
+*/
+const FindFruits = ['Banana', 'Orange', 'Mango', 'Apple']
+const FindResult = FindFruits.find((i) => i === 'Mango');
+const FindResultIndex = FindFruits.findIndex((i) => i === 'Mango');
 
-// console.log(`Find methods -- ${FindResult}`)         // Output: Mango
-// console.log(FindResultIndex)                         // Output: 2
-
-
-// /*
-// Filter Methods------------------
-// -- The filter method creates a new array with all elements that pass the test implemented by the provided function.
-
-// -- Here, it will return multiple elements in a array. 
-// */
-// const FilterFruits = ['Banana', 'Orange', 'Mango', 'Apple']
-// const FilterResult = FilterFruits.filter((i) => i === 'Mango');
-// const FilterMultipleResult = FilterFruits.filter((i) => i === 'Mango' || i === 'Apple');
-
-// console.log(`Filter Methods -- ${FilterResult}`)         // Output: ['Mango']
-// console.log(FilterMultipleResult)                       // Output: ['Mango', 'Apple']
+console.log(`Find methods -- ${FindResult}`)         // Output: Mango
+console.log(FindResultIndex)                         // Output: 2
 
 
-// /*
-// Slice Methods------------------
-// -- The filter method creates a new array with all elements that pass the test implemented by the provided function.
+/*
+Filter Methods------------------
+-- The filter method creates a new array with all elements that pass the test implemented by the provided function.
 
-// -- Here, it will return a new array containing multiple elements from the original array. 
-//    Example: fruits.slice(2, 4) will return ['Mango', 'Apple'].
+-- Here, it will return multiple elements in a array. 
+*/
+const FilterFruits = ['Banana', 'Orange', 'Mango', 'Apple']
+const FilterResult = FilterFruits.filter((i) => i === 'Mango');
+const FilterMultipleResult = FilterFruits.filter((i) => i === 'Mango' || i === 'Apple');
 
-// -- slice(2, 4) Means: slice(Start index, end index)
-
-// -- The first parameter (2) specifies the start index (inclusive), and the second parameter (4) specifies the end index (exclusive).
-// */
-// const SliceFruits = ['Banana', 'Orange', 'Mango', 'Apple']
-// const SliceResult = SliceFruits.slice(2, 4);
-
-// console.log(`Slice method -- ${SliceResult}`)         // Output: ['Mango', 'Apple']
+console.log(`Filter Methods -- ${FilterResult}`)         // Output: ['Mango']
+console.log(FilterMultipleResult)                       // Output: ['Mango', 'Apple']
 
 
-// /*
-// Splice Methods-------------------
-// -- It can change the main array.
+/*
+Slice Methods------------------
+-- The filter method creates a new array with all elements that pass the test implemented by the provided function.
 
-// -- splice(2, 4) Means: splice(index, noOfRemovedItems, aditional element)
+-- Here, it will return a new array containing multiple elements from the original array. 
+   Example: fruits.slice(2, 4) will return ['Mango', 'Apple'].
 
-// -- const result = fruits.splice(1, 4, 'fruit'): Using the splice method to remove 4 elements starting from index 1 and replace them with 'fruit'.
+-- slice(2, 4) Means: slice(Start index, end index)
 
-// -- const result = fruits.splice(1, 4): Here, the splice(1, 4) method removes elements starting from index 1 and removes 4 elements from the array.
-//    Example: fruits.splice(1, 4) will remove 'Orange', 'Mango', and 'Apple' from the array.
-// */
-// const SpliceFruits = ['Banana', 'Orange', 'Mango', 'Apple']
-// const SpliceResult = SpliceFruits.splice(1, 4);
+-- The first parameter (2) specifies the start index (inclusive), and the second parameter (4) specifies the end index (exclusive).
+*/
+const SliceFruits = ['Banana', 'Orange', 'Mango', 'Apple']
+const SliceResult = SliceFruits.slice(2, 4);
 
-// console.log(`Splice method -- ${SpliceResult}`)         // Output: ['Orange', 'Mango', 'Apple']
-// console.log(SpliceFruits)                               // Output: ['Banana']
-
-
-// /*
-// Concat Methods-------------------
-// -- The concat method is used to merge two or more arrays together, creating a new array without modifying the original arrays.
-
-// -- It can't change the main array.
-
-// -- Here, fruits.concat('Fruit') creates a new array by appending the string 'Fruit' to the end of the fruits array.
-// */
-// const ConcatFruits = ['Banana', 'Orange', 'Mango', 'Apple'];
-// const ConcatResult = ConcatFruits.concat('Fruit');
-
-// console.log(`Concat method -- ${ConcatResult}`); // Output: ['Banana', 'Orange', 'Mango', 'Apple', 'Fruit']
+console.log(`Slice method -- ${SliceResult}`)         // Output: ['Mango', 'Apple']
 
 
-// /*
-// Push Methods-------------------
-// -- It can change the main array. Modifies the original array by appending elements.
+/*
+Splice Methods-------------------
+-- It can change the main array.
 
-// -- Syntax: array.push(element1[, ...[, elementN]])
+-- splice(2, 4) Means: splice(index, noOfRemovedItems, aditional element)
 
-// -- Example: PushFruits.push('Fruit') returns the new length of the array.
-// */
-// const PushFruits = ['Banana', 'Orange', 'Mango', 'Apple'];
-// const PushResult = PushFruits.push('Fruit');
+-- const result = fruits.splice(1, 4, 'fruit'): Using the splice method to remove 4 elements starting from index 1 and replace them with 'fruit'.
 
-// console.log(`Push method -- ${PushResult}`); // Output: 5
-// console.log(PushFruits);                     // Output: ['Banana', 'Orange', 'Mango', 'Apple', 'Fruit']
+-- const result = fruits.splice(1, 4): Here, the splice(1, 4) method removes elements starting from index 1 and removes 4 elements from the array.
+   Example: fruits.splice(1, 4) will remove 'Orange', 'Mango', and 'Apple' from the array.
+*/
+const SpliceFruits = ['Banana', 'Orange', 'Mango', 'Apple']
+const SpliceResult = SpliceFruits.splice(1, 4);
+
+console.log(`Splice method -- ${SpliceResult}`)         // Output: ['Orange', 'Mango', 'Apple']
+console.log(SpliceFruits)                               // Output: ['Banana']
 
 
-// /*
-// Reduce Methods-------------------
-// - Iterates over an array and reduces it to a single value.
-// - Syntax: array.reduce(callbackFn[, initialValue])
+/*
+Concat Methods-------------------
+-- The concat method is used to merge two or more arrays together, creating a new array without modifying the original arrays.
 
-// - Example:
-//   - ReduceNumber.reduce((total, currValue) => total + currValue, 0)
-//     - Adds up all the numbers in the array starting from an initial value of 0.
+-- It can't change the main array.
 
-// */
-// const ReduceNumber = [1, 2, 3, 4, 5];
-// const ReduceResult = ReduceNumber.reduce((total, currValue) => total + currValue, 0);
+-- Here, fruits.concat('Fruit') creates a new array by appending the string 'Fruit' to the end of the fruits array.
+*/
+const ConcatFruits = ['Banana', 'Orange', 'Mango', 'Apple'];
+const ConcatResult = ConcatFruits.concat('Fruit');
 
-// console.log(ReduceResult); // Output: 15
+console.log(`Concat method -- ${ConcatResult}`); // Output: ['Banana', 'Orange', 'Mango', 'Apple', 'Fruit']
+
+
+/*
+Push Methods-------------------
+-- It can change the main array. Modifies the original array by appending elements.
+
+-- Syntax: array.push(element1[, ...[, elementN]])
+
+-- Example: PushFruits.push('Fruit') returns the new length of the array.
+*/
+const PushFruits = ['Banana', 'Orange', 'Mango', 'Apple'];
+const PushResult = PushFruits.push('Fruit');
+
+console.log(`Push method -- ${PushResult}`); // Output: 5
+console.log(PushFruits);                     // Output: ['Banana', 'Orange', 'Mango', 'Apple', 'Fruit']
+
+
+/*
+Reduce Methods-------------------
+- Iterates over an array and reduces it to a single value.
+- Syntax: array.reduce(callbackFn[, initialValue])
+
+- Example:
+  - ReduceNumber.reduce((total, currValue) => total + currValue, 0)
+    - Adds up all the numbers in the array starting from an initial value of 0.
+
+*/
+const ReduceNumber = [1, 2, 3, 4, 5];
+const ReduceResult = ReduceNumber.reduce((total, currValue) => total + currValue, 0);
+
+console.log(ReduceResult); // Output: 15
